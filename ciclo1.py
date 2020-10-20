7325721a2a771a4d15322c22338a8ded1be57aeb
Thiare <danaesalinasn@gmail.com>
2020-10-20 00:28:50 -0300

    ejercicio de ciclo en python

--- ciclo1.py
+++ ciclo1.py
@@ -1,6 +1,6 @@
 #crear ciclo for que permita ingresar n temperaturas donde n es un numero ingresado por teclado
 #mostrar cuantas temperaturas estan por sobre cero y cuantas estan bajo o igual a cero
-
+#ejercicio atrasado
 
 sobrecero=0
 bajocero=0
@@ -12,7 +12,7 @@ for i in range(veces): # range tope
 		sobrecero=sobrecero+1
 	else:
 		bajocero=bajocero+1
-		
+
 #mostrar datos
 print("la cantidad de temperaturas mayores a cero es: " + str(sobrecero))
 print("la cantidad de temperaturas menores o iguales a cero es: " + str(bajocero))
